95be58e220d8fc511ab9e24682871bbba26f19dd
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-09-02 22:10:49 +0200

    Replace validate_ calls with types

--- manifests/define.pp
+++ manifests/define.pp
@@ -1,28 +1,18 @@
 # == Define: apticron::define
 #
 define apticron::define (
-  $config_file_path         = undef,
-  $config_file_owner        = undef,
-  $config_file_group        = undef,
-  $config_file_mode         = undef,
-  $config_file_source       = undef,
-  $config_file_string       = undef,
-  $config_file_template     = undef,
+  Optional[Stdlib::Absolutepath] $config_file_path = undef,
+  Optional[String] $config_file_owner = undef,
+  Optional[String] $config_file_group = undef,
+  Optional[String] $config_file_mode = undef,
+  Optional[String] $config_file_source = undef,
+  Optional[String] $config_file_string = undef,
+  Optional[String] $config_file_template = undef,
 
-  $config_file_require      = undef,
+  Optional[String] $config_file_require = undef,
 
-  $config_file_options_hash = $::apticron::config_file_options_hash,
+  Hash $config_file_options_hash = $::apticron::config_file_options_hash,
 ) {
-  if $config_file_path { validate_absolute_path($config_file_path) }
-  if $config_file_owner { validate_string($config_file_owner) }
-  if $config_file_group { validate_string($config_file_group) }
-  if $config_file_mode { validate_string($config_file_mode) }
-  if $config_file_source { validate_string($config_file_source) }
-  if $config_file_string { validate_string($config_file_string) }
-  if $config_file_template { validate_string($config_file_template) }
-
-  if $config_file_require { validate_string($config_file_require) }
-
   $_config_file_path  = pick($config_file_path, "${::apticron::config_dir_path}/${name}")
   $_config_file_owner = pick($config_file_owner, $::apticron::config_file_owner)
   $_config_file_group = pick($config_file_group, $::apticron::config_file_group)
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,33 @@
 # == Class: apticron
 #
 class apticron (
-  $package_ensure           = 'present',
-  $package_name             = $::apticron::params::package_name,
-  $package_list             = $::apticron::params::package_list,
+  Enum['absent', 'latest', 'present', 'purged'] $package_ensure = 'present',
+  String $package_name = $::apticron::params::package_name,
+  Array[String] $package_list = $::apticron::params::package_list,
 
-  $config_dir_path          = $::apticron::params::config_dir_path,
-  $config_dir_purge         = false,
-  $config_dir_recurse       = true,
-  $config_dir_source        = undef,
+  Stdlib::Absolutepath $config_dir_path = $::apticron::params::config_dir_path,
+  Boolean $config_dir_purge = false,
+  Boolean $config_dir_recurse = true,
+  Optional[String] $config_dir_source = undef,
 
-  $config_file_path         = $::apticron::params::config_file_path,
-  $config_file_owner        = $::apticron::params::config_file_owner,
-  $config_file_group        = $::apticron::params::config_file_group,
-  $config_file_mode         = $::apticron::params::config_file_mode,
-  $config_file_source       = undef,
-  $config_file_string       = undef,
-  $config_file_template     = undef,
+  Stdlib::Absolutepath $config_file_path = $::apticron::params::config_file_path,
+  String $config_file_owner = $::apticron::params::config_file_owner,
+  String $config_file_group = $::apticron::params::config_file_group,
+  String $config_file_mode = $::apticron::params::config_file_mode,
+  Optional[String] $config_file_source = undef,
+  Optional[String] $config_file_string = undef,
+  Optional[String] $config_file_template = undef,
 
-  $config_file_require      = $::apticron::params::config_file_require,
+  String $config_file_require = $::apticron::params::config_file_require,
 
-  $config_file_hash         = {},
-  $config_file_options_hash = {},
+  Hash $config_file_hash = {},
+  Hash $config_file_options_hash = {},
 
-  $email                    = "apticron@${::domain}",
-  $email_from               = "root@${::fqdn}",
-  $email_subject            = '[apticron] $SYSTEM: $NUM_PACKAGES package update(s)',
-  $random                   = fqdn_rand('60'),
+  String $email = "apticron@${::domain}",
+  String $email_from = "root@${::fqdn}",
+  String $email_subject = '[apticron] $SYSTEM: $NUM_PACKAGES package update(s)',
+  $random = fqdn_rand('60'),
 ) inherits ::apticron::params {
-  validate_re($package_ensure, '^(absent|latest|present|purged)$')
-  validate_string($package_name)
-  if $package_list { validate_array($package_list) }
-
-  validate_absolute_path($config_dir_path)
-  validate_bool($config_dir_purge)
-  validate_bool($config_dir_recurse)
-  if $config_dir_source { validate_string($config_dir_source) }
-
-  validate_absolute_path($config_file_path)
-  validate_string($config_file_owner)
-  validate_string($config_file_group)
-  validate_string($config_file_mode)
-  if $config_file_source { validate_string($config_file_source) }
-  if $config_file_string { validate_string($config_file_string) }
-  if $config_file_template { validate_string($config_file_template) }
-
-  validate_string($config_file_require)
-
-  validate_hash($config_file_hash)
-  validate_hash($config_file_options_hash)
 
   $config_file_content = extlib::default_content($config_file_string, $config_file_template)
 
@@ -63,9 +42,6 @@ class apticron (
     $config_dir_ensure  = 'directory'
     $config_file_ensure = 'present'
   }
-
-  validate_re($config_dir_ensure, '^(absent|directory)$')
-  validate_re($config_file_ensure, '^(absent|present)$')
 
   anchor { 'apticron::begin': } ->
   class { '::apticron::install': } ->
